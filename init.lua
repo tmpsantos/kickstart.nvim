93e985432bad3532ae55d96fb2a4ca01356e4e02
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2026-09-10 14:07:03 -0300

    Use Tab/S-Tab to cycle blink-cmp completion items

--- init.lua
+++ init.lua
@@ -905,7 +905,10 @@ do
       -- <c-k>: Toggle signature help
       --
       -- See `:help blink-cmp-config-keymap` for defining your own keymap
-      preset = 'default',
+      preset = 'enter',
+
+      ['<S-Tab>'] = { 'select_prev', 'fallback' },
+      ['<Tab>'] = { 'select_next', 'fallback' },
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
