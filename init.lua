780297c6f5466a4c0ae2034123597e87fa81e8f1
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2026-09-10 14:07:15 -0300

    Enable indent_line, autopairs, and neo-tree kickstart plugins

--- init.lua
+++ init.lua
@@ -1027,10 +1027,10 @@ do
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug'
-  -- require 'kickstart.plugins.indent_line'
+  require 'kickstart.plugins.indent_line'
   -- require 'kickstart.plugins.lint'
-  -- require 'kickstart.plugins.autopairs'
-  -- require 'kickstart.plugins.neo-tree'
+  require 'kickstart.plugins.autopairs'
+  require 'kickstart.plugins.neo-tree'
 
   -- NOTE: You can add your own plugins, configuration, etc. in `lua/custom/plugins/*.lua`.
   --
